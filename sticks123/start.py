279da9edcc7801dba2672c54627cd8e6a77904dc
MotiMatika <motimatika@gmail.com>
2022-08-13 22:17:09 +0300

    Create start.py

---
+++ sticks123/start.py
@@ -0,0 +1,26 @@
+print("\nS T I C K S")
+
+print("\n| | | | | | | | | | |")
+print("""\nYou have to remove,
+      one,
+      or two
+      or three sticks.
+      The player who takes the last stick - LOSE""")
+num_of_sticks = 11
+player_decision = input("\nHow many stick you want to remove ?" )
+if player_decision == "1":
+    res = num_of_sticks-player_decision 
+    print("|" * res)
+    print("now we have only",)
+    print("Now the coputer is thinking....")
+    print()
+if num_of_sticks_player == "2": 
+    print("| | | | | | | | |")
+    print("Now the coputer is thinking....")
+if num_of_sticks_player == "3": 
+    print("| | | | | | | |")
+    print("Now the coputer is thinking....") 
+
+       
+
+       
