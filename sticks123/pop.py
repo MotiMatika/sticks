c13b16eb1f9e59df0d09c6df8d9c91f8c070ba02
MotiMatika <motimatika@gmail.com>
2022-08-15 19:03:28 +0300

    Create pop.py

---
+++ sticks123/pop.py
@@ -0,0 +1,26 @@
+print("\nS T I C K S")
+
+print("\n|  |  |  |  |  |  |  |  |  |  |")
+print("""\nYou have to remove,
+      one,
+      or two
+      or three sticks.
+      The player who takes the last stick - LOSE""")
+
+num_of_sticks = 11
+print("\nHere we have",num_of_sticks,"sticks")
+print("\n")
+line = ["|","|","|","|","|","|","|","|","|","|","|" ]
+print(line)
+
+# player_decision = int(input("\nHow many sticks do you want to remove ?" ))
+# if player_decision == 1:
+#     res = num_of_sticks-player_decision
+#     print("now we have only",res) 
+#     print("|" * res)
+#     print("Now the coputer is thinking....")
+#     computer_decision = player_decision
+#     print("The computer decided to remove",computer_decision,"stick")
+#     quantity = num_of_sticks-(player_decision+computer_decision)
+#     print("Now we have only",quantity,"sticks") 
+#     print("|" * quantity)
