f67bc07ffe69c6cd5d09a9ca200c540a5acff73a
MotiMatika <motimatika@gmail.com>
2022-08-18 22:13:35 +0300

    Update tur.py

--- sticks123/tur.py
+++ sticks123/tur.py
@@ -1,122 +1,78 @@
-#ריבוע
+#דוגמא כיצד ניתן לקצר קוד ע"י שימוש בלולאה פור
+#לשירטוט אותו מבנה
+
+#מבוא בסיסי
 import turtle               # allows us to use the turtles library
 tw = turtle.Screen()        # creates a graphics window 
 moti = turtle.Turtle()      # create a turtle named moti 
 moti.color("green")           
 moti.pensize(4) 
-        
-# moti.fd(120)
-# moti.rt(90)
-# moti.fd(120)
-# moti.rt(90)
-# moti.fd(120)
-# moti.rt(90)
-# moti.fd(120)
-# #trig 1
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# #trig 2
-# moti.lt(30)
-# moti.fd(120)
-# moti.lt(120)
-# moti.fd(120)
-# moti.lt(120)
-# moti.fd(120)
 
-# moti.rt(90)
-# moti.fd(120)
+#גירסה ראשונה מסורבלת        
+moti.fd(120)
+moti.rt(90)
+moti.fd(120)
+moti.rt(90)
+moti.fd(120)
+moti.rt(90)
+moti.fd(120)
+#trig 1
+moti.rt(30)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
+#trig 2
+moti.lt(30)
+moti.fd(120)
+moti.lt(120)
+moti.fd(120)
+moti.lt(120)
+moti.fd(120)
 
-# #trig 3
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
+moti.rt(90)
+moti.fd(120)
 
-# moti.rt(180)
-# moti.fd(120)
+#trig 3
+moti.rt(30)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
 
-# #trig 4
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
+moti.rt(180)
+moti.fd(120)
 
-
-#"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-def sq():
-    moti.fd(120)
-    i=0
-    for i <=3:
-        moti.rt(90)
-        moti.fd(120)
-        
-        i=i+1
-    # moti.rt(90)
-    # moti.fd(120)
-    # moti.rt(90)
-    # moti.fd(120)
-def main():    
-   sq()
-        
-    
-
-if __name__ == "__main__":
-    main()
+#trig 4
+moti.rt(30)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
+moti.rt(120)
+moti.fd(120)
 
 
-# #trig 1
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# #trig 2
-# moti.lt(30)
-# moti.fd(120)
-# moti.lt(120)
-# moti.fd(120)
-# moti.lt(120)
-# moti.fd(120)
-
-# moti.rt(90)
-# moti.fd(120)
-
-# #trig 3
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-
-# moti.rt(180)
-# moti.fd(120)
-
-# #trig 4
-# moti.rt(30)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-# moti.rt(120)
-# moti.fd(120)
-
-
-
-
-
-
-
-
-
-
-  
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+#גירסה קצרה
+# def sq():
+#     moti.fd(120)
+#     i=0
+#     for i in range (1,4):
+#         moti.rt(90)
+#         moti.fd(120)
+#         i=i+1        
+#     moti.rt(30)
+# def tri1():
+#     moti.fd(120)
+#     moti.rt(120)
+#     moti.fd(120)        
+#     moti.lt(30)               
+# def main():    
+#     sq()
+#     for i in range(1,5):
+#         tri1()
+# if __name__ == "__main__":
+#     main()      
 turtle.mainloop()           # prevent the window from closing 
