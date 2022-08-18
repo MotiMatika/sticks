6613b44852e9a48fbbb53c9cc86595b2dd2920c5
MotiMatika <motimatika@gmail.com>
2022-08-18 19:02:12 +0300

    Create tur.py

---
+++ sticks123/tur.py
@@ -0,0 +1,122 @@
+#ריבוע
+import turtle               # allows us to use the turtles library
+tw = turtle.Screen()        # creates a graphics window 
+moti = turtle.Turtle()      # create a turtle named moti 
+moti.color("green")           
+moti.pensize(4) 
+        
+# moti.fd(120)
+# moti.rt(90)
+# moti.fd(120)
+# moti.rt(90)
+# moti.fd(120)
+# moti.rt(90)
+# moti.fd(120)
+# #trig 1
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# #trig 2
+# moti.lt(30)
+# moti.fd(120)
+# moti.lt(120)
+# moti.fd(120)
+# moti.lt(120)
+# moti.fd(120)
+
+# moti.rt(90)
+# moti.fd(120)
+
+# #trig 3
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+
+# moti.rt(180)
+# moti.fd(120)
+
+# #trig 4
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+
+
+#"@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+def sq():
+    moti.fd(120)
+    i=0
+    for i <=3:
+        moti.rt(90)
+        moti.fd(120)
+        
+        i=i+1
+    # moti.rt(90)
+    # moti.fd(120)
+    # moti.rt(90)
+    # moti.fd(120)
+def main():    
+   sq()
+        
+    
+
+if __name__ == "__main__":
+    main()
+
+
+# #trig 1
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# #trig 2
+# moti.lt(30)
+# moti.fd(120)
+# moti.lt(120)
+# moti.fd(120)
+# moti.lt(120)
+# moti.fd(120)
+
+# moti.rt(90)
+# moti.fd(120)
+
+# #trig 3
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+
+# moti.rt(180)
+# moti.fd(120)
+
+# #trig 4
+# moti.rt(30)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+# moti.rt(120)
+# moti.fd(120)
+
+
+
+
+
+
+
+
+
+
+  
+turtle.mainloop()           # prevent the window from closing 
