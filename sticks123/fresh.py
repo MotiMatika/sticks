599a1cf6cf9751c090b73edf895f021e92761a0b
MotiMatika <motimatika@gmail.com>
2022-08-22 20:29:49 +0300

    5

--- sticks123/fresh.py
+++ sticks123/fresh.py
@@ -8,21 +8,21 @@ def oppenning():
         or two
         or three sticks.
         The player who takes the last stick - LOSES""")
-def from_15_to_13(player_name,computer_name,num_of_sticks,line): 
+def from_15_to_13(player_name,computer_name,num_of_sticks,line):#הפונקציה מקבלת מהמיין את המשתנים
     print("\nHere we have",num_of_sticks,"sticks\n")
     print(line)
-    player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
-    if player_decision == 1:
-        num_of_sticks = num_of_sticks - 1
-        print("Now we have",num_of_sticks,"sticks")
-        del line[num_of_sticks]
-        print(line)
-        computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
-        num_of_sticks = num_of_sticks - computer_decision
-        print("Now we have",num_of_sticks,"sticks")
-        del line[num_of_sticks]
-        print(line)
-        return line #הפונקציה מחזירה למיין את הרשימה המעודכנת  
+    # player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+    # if player_decision == 1:
+    num_of_sticks = num_of_sticks - 1
+    print("Now we have",num_of_sticks,"sticks")
+    del line[num_of_sticks]
+    print(line)
+    computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+    num_of_sticks = num_of_sticks - computer_decision
+    print("Now we have",num_of_sticks,"sticks")
+    del line[num_of_sticks]
+    print(line)
+    return line
 def from_13_to_9(player_name,computer_name,num_of_sticks,line):
     player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
     if player_decision == 1:
@@ -72,7 +72,7 @@ def from_9_to_5(player_name,computer_name,num_of_sticks,line):
         computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
         num_of_sticks = num_of_sticks - computer_decision
         print("Now we have",num_of_sticks,"sticks")
-        for i in range (2):
+        for i in range (3):
             line.pop()
         print(line)
     if player_decision == 2:
@@ -115,6 +115,7 @@ def from_5_to_1(player_name,computer_name,num_of_sticks,line):
             line.pop()
         print(line)
         print(computer_name," W O N !!!")
+        print("       END of GAME")
     if player_decision == 2:
         num_of_sticks = num_of_sticks - 2
         print("Now we have",num_of_sticks,"sticks")
@@ -128,6 +129,7 @@ def from_5_to_1(player_name,computer_name,num_of_sticks,line):
             line.pop()
         print(line)
         print(computer_name," W O N !!!")
+        print("       END of GAME")
     if player_decision == 3:
         num_of_sticks = num_of_sticks - 3
         print("Now we have",num_of_sticks,"sticks")
@@ -140,9 +142,39 @@ def from_5_to_1(player_name,computer_name,num_of_sticks,line):
         for i in range (1):
             line.pop()
         print(line)
-        print(computer_name," W O N !!!") 
-    return line
-                                     
+        print(computer_name," W O N !!!")
+        print("       END of GAME")
+        
+    return line    
+
+def start(player_name,computer_name,num_of_sticks,line):
+    player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+    if player_decision == 1:
+        from_15_to_13(player_name,computer_name,num_of_sticks,line)
+        from_13_to_9(player_name,computer_name,num_of_sticks,line)
+        from_9_to_5(player_name,computer_name,num_of_sticks,line)
+        from_5_to_1(player_name,computer_name,num_of_sticks,line)  
+              
+    elif player_decision == 3: 
+        num_of_sticks = num_of_sticks - player_decision
+        for i in range (3):
+            line.pop()
+        print("Now we have",num_of_sticks,"sticks")
+        print(line) #ad kan 12
+        computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+        num_of_sticks = num_of_sticks - computer_decision
+        for i in range (3):
+                line.pop()
+        print("Now we have",num_of_sticks,"sticks")
+        print(line) #ad kan 9
+        from_9_to_5(player_name,computer_name,num_of_sticks,line)
+        from_5_to_1(player_name,computer_name,num_of_sticks,line)   
+
+
+
+
+
+
 def main():
     oppenning()
     #השמות בסיסיות
@@ -150,7 +182,7 @@ def main():
     computer_name = "Moti"
     line = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
     num_of_sticks = len(line)
-    
+    start(player_name,computer_name,num_of_sticks,line)
     line = from_15_to_13(player_name,computer_name,num_of_sticks,line)#הפונקציה מתעדכנת ברשימה
     num_of_sticks = len(line) #המשתנה מתעדכן באורך הרשימה החדשה
     
@@ -163,47 +195,4 @@ def main():
     from_5_to_1(player_name,computer_name,num_of_sticks,line)  
 
 if __name__ == "__main__":
-    main()
-#:נשארו המצבים
-# שחקן לוקח 3 בהתחלה  - מצב קצר
-# שחקן לוקח 2 בהתחלה - מצב ארוך
-
-# שחקן לוקח 3 בהתחלה  - מצב קצר
-#
-
-# print("\n|  |  |  |  |  |  |  |  |  |  |")
-
-# print("""\nYou have to remove,
-#       one,
-#       or two
-#       or three sticks.
-#       The player who takes the last stick - LOSE""")
-
-# player_name   = "Tomer"
-# computer_name = "Moti"
-# num_of_sticks = 15
-# print("\nHere we have",num_of_sticks,"sticks")
-# print("\n")
-# line = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-# print(line)
-
-
-# player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
-# if player_decision == 3:
-#     num_of_sticks = num_of_sticks - player_decision
-#     for i in range (3):
-#             line.pop()
-#     print("Now we have",num_of_sticks,"sticks")
-#     print(line)
-#     #ad kan 12
-#     computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
-#     num_of_sticks = num_of_sticks - computer_decision
-#     for i in range (3):
-#             line.pop()
-#     print("Now we have",num_of_sticks,"sticks")
-#     print(line)
-#     from_9_to_5(player_name,computer_name,num_of_sticks,line)
-#     from_5_to_1(player_name,computer_name,num_of_sticks,line)
-    #ad kan 9
-    #oved
-
+   main()    
