844d00d6938c3707635804e84a5fc37d7b88d95c
MotiMatika <motimatika@gmail.com>
2022-08-27 21:22:58 +0300

    5

--- sticks123/fresh.py
+++ sticks123/fresh.py
@@ -1,4 +1,5 @@
-def oppenning():
+import random as rd
+def oppenning(line):
     print("\nS T I C K S")
 
     print("\n|  |  |  |  |  |  |  |  |  |  |")
@@ -8,44 +9,71 @@ def oppenning():
         or two
         or three sticks.
         The player who takes the last stick - LOSES""")
-    line = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    
     print("\n"+"This is the line of the sticks")
     print("\n",line)
-def for_player(player_decision,line):
+    return line
+def type_player(player_decision,line):#מכילה הוראות צמצום רשימה והדפסתה עבור השחקן
     for i in range (player_decision):
         line.pop()
     print(line) 
-def from_14_to_13(player_name,computer_name):
-    line = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-    player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
-    if player_decision == 1:   #נותרו 14
-        print("\n"+"This is the new line of the sticks")
-        print("\n",line)
-        for_player(player_decision,line)
+def from_14_to_13(player_decision,line):
+    print("\n"+"This is the new line of the sticks")
+    type_player(player_decision,line)
+    num_of_sticks = 14
+    
+    return num_of_sticks,line 
+def Rand_Com(num_of_sticks,computer_name,compuer_decision,line):
+    num_of_sticks = num_of_sticks - computer_decision
+    print("\n",computer_name, "decided to remove",computer_decision)
+    print("Now we have",num_of_sticks,"sticks")
+    type_computer(computer_decision,line)
+def from_13_to_9(computer_name,nume_of_sticks,line):
+    computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+    num_of_sticks = num_of_sticks - computer_decision
+    print("Now we have",num_of_sticks,"sticks")
+    for i in range (computer_decision):
+        line.pop()
+    print(line)
 
 
 
 def main():
          #השמות בסיסיות
     player_name   = "Dror"
-    computer_name = "Moti"    
-    oppenning()
-    from_14_to_13(player_name,computer_name)
+    computer_name = "Moti"
+    line = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]    
+    oppenning(line)
     
-    #start(player_name,computer_name)
-      
+    player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+    if player_decision == 1:       #נותרו 14
+        from_14_to_13(player_decision,line)
+        
+        from_13_to_9(computer_name,line)
+        
+        #line=Rand_Com(13,computer_name,rd.randint(1,3),line)     
 
 if __name__ == "__main__":
-   main() 
+        main() 
     
 
-
-
-
-
-
-
-
+# def from_13_to_9(computer_name):
+#     computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+#     num_of_sticks = num_of_sticks - computer_decision
+#     print("Now we have",num_of_sticks,"sticks")
+#     type_computer
+#     for i in range (1):
+#         line.pop()
+#     print(line)
+#     line = [1,2,3,4,5,6,7,8,9,10,11,12,13]
+#     num_of_sticks = len(line)
+#     player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+#     if player_decision == 1:
+#         num_of_sticks = num_of_sticks - player_decision
+#         print("Now we have",num_of_sticks,"sticks")
+#         for i in range (player_decision):
+#             line.pop()
+#         print(line)
 
 
 
@@ -86,9 +114,7 @@ if __name__ == "__main__":
 #     #start(player_name,computer_name)
       
 
-# if __name__ == "__main__":
-#    main() 
-    
+
 #def from_13_to_9(player_name,computer_name):
 #     player_name   = "Dror"
 #     computer_name = "Moti"
---
+++ sticks123/r.py
@@ -0,0 +1,9 @@
+
+def yair(y):
+    y=y-2
+    return y
+def moti(m):
+    m=m+4
+    
+    return m
+print(moti(6)+yair(8))
