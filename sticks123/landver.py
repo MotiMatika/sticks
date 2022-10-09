68e1ef9c459be649759a1fbdaeb2b52ace792280
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-10-09 12:43:16 +0300

    Create landver.py

---
+++ sticks123/landver.py
@@ -0,0 +1,12 @@
+def main():
+
+    player_name   = "d"
+    computer_name = "m"
+    line = [1,2,3,4,5,6,7]    
+    print (line)
+    
+    
+    
+    if __name__ == "__main__":
+        main()
+                 
