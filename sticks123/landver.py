605a25f5ff2da1d2b416b7909b24562a9bc43326
MotiMatika <98161728+MotiMatika@users.noreply.github.com>
2022-10-09 13:38:49 +0300

    Update landver.py

--- sticks123/landver.py
+++ sticks123/landver.py
@@ -1,12 +1,59 @@
-def main():
+def p_1(player_decision,line):#שחקן מוחק מקל בודד
+    print("\n"+"This is the new line of the sticks")
+    for i in range (player_decision):
+        line.pop()
+    print(line)
+    return line 
 
-    player_name   = "d"
-    computer_name = "m"
-    line = [1,2,3,4,5,6,7]    
-    print (line)
+def c_1(computer_decision,line):#מחשב מוחק מקל בודד
+    print("\n"+"This is the new line of the sticks")
+    for i in range (computer_decision):
+        line.pop()
+    print(line)
+    return line 
+
+def mevi_oto_3(player_name,computer_name,line):
+    if player_decision == 1:#3 long 
+        p_1(player_decision,line)
+        computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+        c_1(computer_decision,line)   
+        print(computer_name,"W O N !!!")       
+    elif player_decision == 2:#1 long 
+        p_1(player_decision,line)
+        print(player_name,"W O N !!!") 
+
+
+def mevi_oti_3(player_name,computer_name,line):
+    if computer_decision == 1:#3 long 
+        c_1(computer_decision,line)
+        player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+        p_1(player_decision,line)   
+        print(player_name,"W O N !!!")       
+    elif computer_decision == 2:#1 long 
+        c_1(computer_decision,line)
+        print(computer_name,"W O N !!!") 
+
+
+
+
+
+# def main():
+
+player_name   = "d"
+computer_name = "m"
+line = [1,2,3]    
+
+length = len(line)
+
+if  length <= 3: 
+    turn = input("who's turn ?")  
+    if turn == "d":
+        player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+        mevi_oto_3(player_name,computer_name,line) 
+    elif turn == "m":
+        computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+        mevi_oti_3(player_name,computer_name,line)
     
-    
-    
-    if __name__ == "__main__":
-        main()
+    # if __name__ == "__main__":
+    #     main()
                  
