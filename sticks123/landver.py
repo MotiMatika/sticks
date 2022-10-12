a3fd64eb1fe12af6a7805d4ca4ea8a76cfaf6447
MotiMatika <motimatika@gmail.com>
2022-10-12 21:57:49 +0300

    Update landver.py

--- sticks123/landver.py
+++ sticks123/landver.py
@@ -4,7 +4,6 @@ def p_1(player_decision,line):          #שחקן מוחק מקל בודד
         line.pop()
     length = len(line)
     print(line)
-    print(length)
     return line,length 
 
 def p_2(player_decision,line):          #שחקן מוחק  2 מקלות 
@@ -19,7 +18,8 @@ def p_3(player_decision,line):          #שחקן מוחק  3 מקלות
     for i in range (player_decision):
         line.pop()
     print(line)
-    return line 
+    length = len(line)
+    return line , length
 
 def c_1(computer_decision,line):#מחשב מוחק מקל בודד
     print("\n"+"This is the new line of the sticks")
@@ -35,57 +35,54 @@ def c_2(computer_decision,line):#מחשב מוחק 2 מקלות
     print(line)
     return line 
 
-def mevi_oto_3(player_decision,player_name,computer_name,line):
-    if player_decision == 1:#3 long 
+def p_starts_from_3_to_1(player_name,computer_name,line):
+    player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
+    if player_decision == 1:                #2 long 
         p_1(player_decision,line)
         computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
-        c_1(computer_decision,line)   
-        print(computer_name,"W O N !!!")       
-    elif player_decision == 2:#1 long 
+        if computer_decision == 1:          #1 long
+            c_1(computer_decision,line)   
+            print(computer_name,"W O N !!!")       
+    elif player_decision == 2:              #1 long 
         p_2(player_decision,line)
         print(player_name,"W O N !!!") 
 
-def mevi_oti_3(computer_decision,player_decision,player_name,computer_name,line):
-    if computer_decision == 1:#3 long 
+def c_starts_from_3_to_1(player_name,computer_name,line):
+    computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
+    if computer_decision == 1:          #2 long 
         c_1(computer_decision,line)
         player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
         p_1(player_decision,line)   
         print(player_name,"W O N !!!")       
-    elif computer_decision == 2:#1 long 
+    elif computer_decision == 2:        #1 long 
         c_2(computer_decision,line)
         print(computer_name,"W O N !!!") 
-
-def finish():
-    turn = input("\nwho's turn ?")  
-    if turn == "d":
-        player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
-        mevi_oto_3(player_decision,player_name,computer_name,line) 
-    elif turn == "m":
-        computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
-        mevi_oti_3(computer_decision,player_name,computer_name,line)
-
 
 
 # def main():
 
 player_name   = "d"
 computer_name = "m"
-line = [1,2,3]    
+line = [1,2,3,4,5,6]    
 
 length = len(line)
 
 if  length <= 3: 
-    finish()
+    turn = input("\nwho's turn ?")  
+    if turn == "d":
+        p_starts_from_3_to_1(player_name,computer_name,line) 
+    elif turn == "m":
+        c_starts_from_3_to_1(player_name,computer_name,line)
 else:
     turn = input("\nwho's turn ?")  
     if turn == "d":
         player_decision = int(input("\n" + player_name + ",how many sticks do you want to remove ? " ))
-        if player_decision   == 1:                             
-            p_1(player_decision,line)
+        if player_decision   == 3:                             
+            p_3(player_decision,line)
             if  length <= 3:
-                finish()
-        #     else:
-        #         pass
+                c_starts_from_3_to_1(player_name,computer_name,line)
+            else:
+                print("yes")
         # elif player_decision   == 2:                             
         #     p_2(player_decision,line)
         # elif player_decision   == 3:                             
