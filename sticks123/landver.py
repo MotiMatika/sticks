9ab76fcbc178909e39cb7562b14e28760805aba9
MotiMatika <motimatika@gmail.com>
2022-10-12 20:52:48 +0300

    Update landver.py

--- sticks123/landver.py
+++ sticks123/landver.py
@@ -28,14 +28,21 @@ def c_1(computer_decision,line):#מחשב מוחק מקל בודד
     print(line)
     return line 
 
-def mevi_oto_3(player_name,computer_name,line):
+def c_2(computer_decision,line):#מחשב מוחק 2 מקלות
+    print("\n"+"This is the new line of the sticks")
+    for i in range (computer_decision):
+        line.pop()
+    print(line)
+    return line 
+
+def mevi_oto_3(player_decision,player_name,computer_name,line):
     if player_decision == 1:#3 long 
         p_1(player_decision,line)
         computer_decision = int(input("\n" + computer_name + ",how many sticks do you want to remove ? " ))
         c_1(computer_decision,line)   
         print(computer_name,"W O N !!!")       
     elif player_decision == 2:#1 long 
-        p_1(player_decision,line)
+        p_2(player_decision,line)
         print(player_name,"W O N !!!") 
 
 def mevi_oti_3(computer_decision,player_decision,player_name,computer_name,line):
@@ -45,7 +52,7 @@ def mevi_oti_3(computer_decision,player_decision,player_name,computer_name,line)
         p_1(player_decision,line)   
         print(player_name,"W O N !!!")       
     elif computer_decision == 2:#1 long 
-        c_1(computer_decision,line)
+        c_2(computer_decision,line)
         print(computer_name,"W O N !!!") 
 
 def finish():
@@ -63,7 +70,7 @@ def finish():
 
 player_name   = "d"
 computer_name = "m"
-line = [1,2,3,4]    
+line = [1,2,3]    
 
 length = len(line)
 
